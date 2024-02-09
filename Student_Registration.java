95b7338320e69265adb1dfc7ff7de05a78ea26d2
Bokkaramyasri <156409182+Bokkaramyasri@users.noreply.github.com>
2024-02-09 11:50:21 +0530

    import java.util.Scanner; public class Main{     public static void main(String  []args)     {         Scanner sc=new Scanner(System.in);         int t=sc.nextInt();         for(int i=0;i<t;i++)         {         int n=sc.nextInt();         int m=sc.nextInt();         int k=sc.nextInt();         if((m-k)<n)         {             System.out.println("NO");         }         else System.out.println("YES");     }     } }

---
+++ Student_Registration.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String  []args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int k=sc.nextInt();
+        if((m-k)<n)
+        {
+            System.out.println("NO");
+        }
+        else System.out.println("YES");
+    }
+    }
+}
